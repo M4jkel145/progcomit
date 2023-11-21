e274b83d88e1d8a6ba93d1089e1be35db6dcfbbe
Majkel isabar <majkel.isabar@elev.ga.ntig.se>
2023-11-21 12:48:54 +0100

    hejhw

---
+++ Vinterprojektet/Vinterprojektet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+string name = "Majkel";
+string favoriteSport = "Fotboll";
+string petName = "Kahpe";
+
+Console.WriteLine("Majkel");
+name = Console.ReadLine();
+
+Console.WriteLine("Fotboll");
+favoriteSport = Console.ReadLine();
+
+Console.WriteLine("Kahpe");
+petName = Console.ReadLine();
+
+//------------------------------------------------------------------------------
+
+Console.WriteLine($"Hördu Majkel, jag har hört att du är jättedålig på Fotboll.");
+Console.WriteLine($"Du är så dålig att till och med kahpe skrattar åt dig!");
+
+Console.WriteLine("Tryck på ENTER för att avsluta.");
+Console.ReadLine();
